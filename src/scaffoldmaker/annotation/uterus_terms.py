f78032d97b19c433fe41757b1fe1db99b7d1d710
mlin865 <mlin865@aucklanduni.ac.nz>
2024-02-08 14:59:17 +1300

    Inital commit of uterus2

--- src/scaffoldmaker/annotation/uterus_terms.py
+++ src/scaffoldmaker/annotation/uterus_terms.py
@@ -4,12 +4,24 @@ Common resource for uterus annotation terms.
 
 # convention: preferred name, preferred id, followed by any other ids and alternative names
 uterus_terms = [
-    ("uterus", "UBERON:0000995"),
+    ("body of uterus", "UBERON:0009853", "FMA:17739", "ILX:0730129"),
+    ("broad ligament of uterus", "UBERON:0012332", "FMA:16516", "ILX:0733266"),
+    ("external cervical os", "UBERON:0013760", "FMA:76836", "ILX:0736534"),
+    ("fallopian tube", "UBERON:0003889", "FMA:18245", "ILX:0724851"),
+    ("internal cervical os", "UBERON:0013759", "FMA:17747", "ILX:0729495"),
+    ("left broad ligament of uterus", "None"),
+    ("left round ligament of uterus", "None"),
+    ("myometrium", "UBERON:0001296", "FMA:17743", "	ILX:0735601"),
+    ("pubocervical ligament (TA98)", "ILX:0743760"),
+    ("right broad ligament of uterus", "None"),
+    ("right round ligament of uterus", "None"),
+    ("round ligament of uterus", "UBERON:0006589", "ILX:0725512"),
+    ("uterine cervix", "UBERON:0000002","FMA:17740", "ILX:0724162"),
+    ("uterine wall", "UBERON:0000459", "FMA:17560", "ILX:0735839"),
+    ("uterus", "UBERON:0000995", "FMA:17558", "ILX:0726002"),
+    ("vagina", "UBERON:0000996", "FMA:19949", "ILX:0736016"),
+    ("vagina orifice", "UBERON:0012317", "FMA:19984", "ILX:0729556"),
     ("serosa of uterus", "UBERON:0001297"),
-    ("body of uterus", "UBERON:0009853"),
-    ("cervix of uterus", "ILX:0745917 "),
-    ("uterine cervix", "UBERON:0000002"),
-    ("uterine wall", "UBERON:0000459"),
     ("uterine horn", "UBERON:000224"),
     ("uterine lumen", "UBERON:0013769"),
     ("left uterine horn", "UBERON:0009020"),
@@ -28,8 +40,7 @@ uterus_terms = [
     ("dorsal top right horn", "None"),
     ("ventral top right horn", "None"),
     ("dorsal top left horn", "None"),
-    ("ventral top left horn", "None"),
-    ("vagina", "None")
+    ("ventral top left horn", "None")
 ]
 
 def get_uterus_term(name: str):
